02cb326299f2aebd2235da0df43dbf3715fc7918
Tejas Sinroja <tejassinroja@gmail.com>
2025-07-06 20:03:25 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnClassifier.pipeline.stage_02_prepare_model_base_model import PrepareBaseModelTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 
 try:
     logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
     data_ingestion_pipeline = DataIngestionTrainingPipeline()
     data_ingestion_pipeline.main()
+    logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<<<<")
+    prepare_base_model_pipeline = PrepareBaseModelTrainingPipeline()
+    prepare_base_model_pipeline.main()
     logger.info(f">>>>>>>>>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<<<<<<<<")
 except Exception as e:
     logger.exception(e)
--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,6 +1,4 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("config/config.yaml")
-print("CONFIG_FILE_PATH:", CONFIG_FILE_PATH)
 PARAMS_FILE_PATH = Path("params.yaml")
-print("PARAMS_FILE_PATH:", PARAMS_FILE_PATH)
